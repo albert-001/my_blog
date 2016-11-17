f09fe466306655b164547ae99362a273d4fc7390
albert-001 <albert-001@users.noreply.github.com>
2016-11-17 10:19:10 +0800

    get data from backend

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def bike_key(request):
     return render(request, 'bike_keys.html', {})
+
+def keys(request):
+    k = {"keys":[["65270666","8106"],["417052","0864"],["382630","1392"],
+            ["354840","8824"],["336976","1267"],["163166","0610"],
+            ["326216","6721"]]}
+    return JsonResponse(k, safe=False)
