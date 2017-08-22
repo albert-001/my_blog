44238c1248d65642709bf62788bfe013b0ea8382
hao hu <huhao.bupt@gmail.com>
2017-08-22 09:25:55 +0800

    add publish and __str__ methods to new models class

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,6 @@ class Post(models.Model):
 	date = models.DateTimeField(default=timezone.now)
 
 	def publish(self):
-		self.date = timezone.now()
 		self.save()
 
 	def __str__(self):
@@ -19,6 +18,18 @@ class News(models.Model):
 	date = models.DateTimeField(default=timezone.now)
 	liner = models.CharField(max_length=100)
 
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.liner
+
 class Link(models.Model):
 	text = models.CharField(max_length=100)
 	url = models.CharField(max_length=100)
+
+	def publish(self):
+		self.save()
+
+	def __str__(self):
+		return self.text
