695b4959d704a570861135fef32909cd774ebc50
hao hu <huhao.bupt@gmail.com>
2017-08-22 10:12:25 +0800

    fix an error in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^(?P<page_number>\d+)/', views.page, name='page'),
     url(r'^article/(?P<article_number>\d+)/', views.article, name='article'),
     url(r'^news/', views.news, name='news'),
-    url(r'^links/', views.news, name='links'),
+    url(r'^links/', views.links, name='links'),
 ]
